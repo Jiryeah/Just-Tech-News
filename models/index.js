348814818383711c9461af8e09bf5344e0979fb3
Chidori <business.faissal@outlook.com>
2022-01-29 18:45:13 -0600

    completed CRUD routes for Post

--- models/index.js
+++ models/index.js
@@ -1,6 +1,16 @@
-//* Imports User model
+//* Imports User/Post model
 const User = require(`./User`);
 const Post = require(`./Post`)
+
+// create associations
+User.hasMany(Post, {
+  foreignKey: `user_id`,
+});
+
+Post.belongsTo(User, {
+  foreignKey: `user_id`,
+  onDelete:`cascade`,
+});
 
 //* Exports an object as a property
 module.exports = { User, Post };
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,49 +1,49 @@
-const router = require('express').Router();
-const { User } = require('../../models');
+const router = require("express").Router();
+const { User } = require("../../models");
 
 // GET /api/users
 //* Retrieves all users
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // Access our User model and run .findAll() method)
   //* find.All() is the JS equivalent of SQL SELECT * FROM user;.
   User.findAll({
     //* password is within an array to allow us to add more password columns as we go
-    attributes: { exclude: [`password`] }
+    attributes: { exclude: [`password`] },
   })
-  .then(dbUserData => res.json(dbUserData))
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err)
-  });
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // GET /api/users/1
 //* Finds one user by id
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   //* findOne() w/ 'where' identifier, is the JS equivalent of SQL
   //* SELECT * FROM users WHERE id = 1;.
   User.findOne({
     attributes: { exclude: [`password`] },
     where: {
-      id: req.params.id
-    }
+      id: req.params.id,
+    },
   })
-  .then(dbUserData => {
-    if (!dbUserData) {
-      res.status(404).json({ message: 'No user found with this id' });
-      return;
-    }
-    res.json(dbUserData);
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with this id" });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // POST /api/users
 //* Creates a new user.
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   //* creat() methods allows use to insert data; similar to SQL
   //* INSERT INTO users (username, email, password)
@@ -51,13 +51,13 @@ router.post('/', (req, res) => {
   User.create({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password
+    password: req.body.password,
   })
-  .then(dbUserData => res.json(dbUserData))
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // User login
@@ -65,14 +65,14 @@ router.post('/', (req, res) => {
 //* whereas the POST method carries the request parameter in req.body, which makes it a more secure way to send client
 //* from the user to the server.
 router.post(`/login`, (req, res) => {
-// expects {email: 'lernantino@gmail.com', password: 'password1234'}
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
   User.findOne({
     where: {
-      email: req.body.email
-    }
-  }).then(dbUserData => {
+      email: req.body.email,
+    },
+  }).then((dbUserData) => {
     if (!dbUserData) {
-      res.status(400).json({ message: `No user with that email address! `});
+      res.status(400).json({ message: `No user with that email address! ` });
       return;
     }
 
@@ -81,14 +81,14 @@ router.post(`/login`, (req, res) => {
       res.status(400).json({ message: `Incorrect Password` });
       return;
     }
-    
+
     res.json({ user: dbUserData, message: ` You are now logged in!` });
   });
 });
 
 // PUT /api/users/1
-//* Allows user to update by their info id 
-router.put('/:id', (req, res) => {
+//* Allows user to update by their info id
+router.put("/:id", (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
   // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
@@ -98,46 +98,45 @@ router.put('/:id', (req, res) => {
   User.update(req.body, {
     individualHooks: true,
     where: {
-      id: req.params.id
-    }
+      id: req.params.id,
+    },
   })
-  .then(dbUserData => {
-    if (!dbUserData[0]) {
-      res.status(404).json({ message: 'No user found with this id' });
-      return;
-    }
-    res.json(dbUserData);
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+    .then((dbUserData) => {
+      if (!dbUserData[0]) {
+        res.status(404).json({ message: "No user found with this id" });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // DELETE /api/users/1
-//* Allows user to delete their information, uses the id to identify the user. 
-router.delete('/:id', (req, res) => {
+//* Allows user to delete their information, uses the id to identify the user.
+router.delete("/:id", (req, res) => {
   //* destroy() method w/ 'where' identifier, allows us to delete the specified data.
   User.destroy({
     where: {
-      id: req.params.id
-    }
+      id: req.params.id,
+    },
   })
-  .then(dbUserData => {
-    if (!dbUserData) {
-      res.status(404).json({ message: 'No user found with this id' });
-      return;
-    }
-    res.json(dbUserData);
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with this id" });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
-
 
 //* API Architectural Pattern REST = REpresentational State Transfer.
 //* APIs built following this pattern as what's known as RESTful APIs
