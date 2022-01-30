4161b13593ad8dfa1cfcef3bc7051ad0193c2698
Chidori <business.faissal@outlook.com>
2022-01-29 20:11:38 -0600

    created the Vote model

--- models/Vote.js
+++ models/Vote.js
@@ -12,5 +12,30 @@ Vote.init(
       autoIncrement: truncate,
     },
     //
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: `user`,
+        key: `id`
+      }
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false, 
+      references: {
+        model: `post`,
+        key: `id`
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: `vote`
   }
-)
+);
+
+module.exports = Vote;
