651b2578e83346dd5a2bc24f9e4d83b33427b718
Chidori <business.faissal@outlook.com>
2022-02-01 15:08:01 -0600

    created the homepage with handlebars.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
+const homeRoutes = require(`./home-routes`);
 
 const apiRoutes = require('./api');
 
 //* Collects packaged API endpoints and prefixes them with '/api'.
-router.use('/api', apiRoutes);
+router.use(`/api`, apiRoutes);
+router.use(`/`, homeRoutes);
 
 //* If request is made to an endpoint that does not exist, a 404 error will be received.
 router.use((req, res) => {
