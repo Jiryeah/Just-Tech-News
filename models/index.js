6d82478e7f95564d40e817d027eecf8e0cbc16b6
Chidori <business.faissal@outlook.com>
2022-02-01 02:41:32 -0600

    created comments route and model

--- models/index.js
+++ models/index.js
@@ -1,9 +1,18 @@
 //* Imports User/Post model
 const Vote = require(`./Vote`);
 const User = require(`./User`);
-const Post = require(`./Post`)
+const Post = require(`./Post`);
+const Comment = require(`./Comments`);
 
 // create associations
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
 //* This foreign key ties both tables together.
 //* Almost like a 'common denominator'. 
 User.hasMany(Post, {
@@ -14,12 +23,20 @@ User.hasMany(Vote, {
   foreignKey: `user_id`
 });
 
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
 //* will allow us to see how many posts the user has voted on.
 //* 'belongsToMany()' allows the models to query each others information in the context of a vote.
 User.belongsToMany(Post, {
   through: Vote,
   as: 'voted_posts',
   foreignKey: `user_id`
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
 });
 
 Post.hasMany(Vote, {
@@ -46,4 +63,4 @@ Vote.belongsTo(Post, {
 });
 
 //* Exports an object as a property
-module.exports = { User, Post, Vote };
+module.exports = { User, Post, Vote, Comment };
